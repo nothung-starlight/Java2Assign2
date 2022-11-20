f1a6cc4bd3cd84d53cd9e5ade7ec4ce9cd608517
xlch555 <84723829+xlch555@users.noreply.github.com>
2022-11-20 16:08:16 +0800

    first

---
+++ serverThread.java
@@ -0,0 +1,35 @@
+package my;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class serverThread extends Thread{
+    Socket client;
+    server server;
+    public serverThread(Socket client,server server){
+        this.client=client;
+        this.server=server;
+    }
+    public void run(){
+        try {
+
+
+        String inStr;
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        while ((inStr = in.readLine()) != null){
+            System.out.println(inStr);
+            server.rec(inStr,client);
+        }
+        }
+        catch (IOException e) {
+            try {
+                server.ex(client);
+                System.out.println("break");
+            } catch (IOException ignored) {
+
+            }
+        }
+    }
+}
